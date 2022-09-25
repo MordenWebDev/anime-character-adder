ff4e94fee1baf9edc2089164a05258327cc0749f
MordenWebDev <95534788+MordenWebDev@users.noreply.github.com>
2022-09-25 17:08:26 +0530

    edit and search functionality added

--- script/index.js
+++ script/index.js
@@ -19,17 +19,17 @@ const cardcontent = ({ id, url,type, title, description }) => {
  
  
   <div class="card-body">
-    <h5 class="card-title">${title} <span class="badge bg-primary rounded-pill">${type}</span></h5>
-    
-    <p  class="card-text">${description.substr(1, 50)}</p>
+    <h5 class="card-title">${title}</h5>
+    <span class="badge bg-primary rounded-pill">${type}</span>
+    <p  class="card-text">${description.substr(0, 50)}</p>
   </div>
 
   <div class="card-body d-flex align-items-center justify-content-between">
   
-    <a href="#" class="card-link btn btn-dark " data-bs-toggle="modal" data-bs-target="#view__more_personmodel" id=${id}>view more</a>
+    <button href="#" class="card-link btn btn-dark " id=${id} name=${id}  data-bs-toggle="modal" data-bs-target="#view__more_personmodel" onclick='alternativeopen(${id})'>view more</button>
     <div class="edit d-flex align-items-center edit-delete ">
-<a href="#" name=${id}><i class="fa-solid fa-pen-to-square" name=${id}></i> Edit</a>
-<a href="#" name=${id} class="text-danger"><i name=${id} class="fa-solid fa-trash"></i> Delete</a>
+<a href="#" onclick=' editpersoninfo.apply(this,arguments)' name=${id}><i class="fa-solid fa-pen-to-square" name=${id}></i> Edit</a>
+<a href="#" name=${id} class="text-danger" onclick='deletetheperson.apply(this,arguments)'><i name=${id} class="fa-solid fa-trash"></i> Delete</a>
     </div>
   </div>
 </div>
@@ -104,4 +104,137 @@ const handelsubmit = () => {
             id,
     })
     updatelocalstorage()
+}
+
+const openpersoninfo = (e) => {
+    console.log(e)
+    if (!e) e = window.event;
+    const getperson = data.persons.find(({ id }) => id === e.target.id);
+    // detailinfo.innerHTML=viewmoreinfo(getperson)
+    console.log(getperson)
+}
+
+
+const alternativeopen = (id) => {
+    console.log(id)
+    let searchedid = id;
+    // if (!e) e = window.event;
+    const getperson = data.persons.find(({ id }) => id === searchedid);
+   
+    detailinfo.innerHTML=viewmoreinfo(getperson)
+    // console.log(detailinfo)
+    // console.log(getperson)
+}
+
+
+const deletetheperson = (e) => {
+    if (!e) e = window.event;
+    const personid = e.target.name;
+    console.log(personid)
+    const removerdperson = data.persons.filter(x => {
+        return x.id!=personid
+    })
+    data.persons = removerdperson
+    updatelocalstorage()
+    let element = document.getElementById(personid);
+element.parentNode.removeChild(element);
+    
+}
+
+const searchperson = (e) => {
+        if (!e) e = window.event;
+    while (cards.firstChild) {
+    cards.removeChild(cards.firstChild)
+}
+    const resultdata = data.persons.filter(({ title }) => {
+     return   title.toLowerCase().includes(e.target.value.toLowerCase())
+    })
+
+    resultdata.map((x) => {
+       return  cards.insertAdjacentHTML('beforeend', cardcontent(x))
+    });
+}
+
+
+const editpersoninfo = (e) => {
+    if (!e) e = window.event;
+    const targetid = e.target.name;
+
+   let element = document.getElementById(targetid);
+    console.log(element.firstElementChild.childNodes[3].lastElementChild)
+    let parentNode = e.target.parentNode;
+    let title=element.firstElementChild.childNodes[3].childNodes[1];
+    let description=element.firstElementChild.childNodes[3].lastElementChild;
+    let type = element.firstElementChild.childNodes[3].childNodes[3];
+    let submitbutton = element.firstElementChild.childNodes[5].firstElementChild;
+    title.setAttribute("contenteditable", "true");
+type.setAttribute("contenteditable", "true");
+description.setAttribute("contenteditable", "true");
+    submitbutton.setAttribute("onclick", `saveedit(this,arguments)`)
+    submitbutton.removeAttribute('data-bs-toggle')
+        submitbutton.removeAttribute('data-bs-target')
+submitbutton.innerHTML='Save changes'
+}
+
+const saveedit = (e) => {
+    if (!e) e = window.event;
+    
+    
+
+  console.log(e)
+    const targetid = e.getAttribute('id');
+    console.log(targetid)
+ let element = document.getElementById(targetid);
+    console.log(element.firstElementChild.childNodes[3].lastElementChild)
+    // let parentNode = e.target.parentNode;
+    let title=element.firstElementChild.childNodes[3].childNodes[1];
+    let description=element.firstElementChild.childNodes[3].lastElementChild;
+    let type = element.firstElementChild.childNodes[3].childNodes[3];
+    let submitbutton = element.firstElementChild.childNodes[5].firstElementChild;
+
+    const updateddata = {
+        title:title.innerText,
+        description:description.innerText,
+        type:type.innerText,
+        
+    }
+
+    // console.log(updateddata)
+
+    let datacopy = data.persons;
+
+    datacopy = datacopy.map(x => 
+     
+       
+        x.id == targetid ? {
+            id: x.id,
+            url: x.url,
+            title: updateddata.title,
+            description: updateddata.description,
+            type: updateddata.type
+        }
+            : x
+        
+    
+     
+    
+    )
+    console.log(datacopy,"last")
+
+    data.persons = datacopy;
+    
+    updatelocalstorage();
+
+      title.setAttribute("contenteditable", "false");
+type.setAttribute("contenteditable", "false");
+    description.setAttribute("contenteditable", "false");
+       
+     submitbutton.innerHTML = 'view more'
+      submitbutton.setAttribute('data-bs-toggle',"modal")
+    submitbutton.setAttribute('data-bs-target',"#view__more_personmodel")
+    
+     submitbutton.setAttribute("onclick", ` alternativeopen(${targetid})`)
+   
+
+
 }
